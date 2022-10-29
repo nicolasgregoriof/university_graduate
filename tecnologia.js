5d5bad4c06fb051121cfa1810d6b45fda23faaa1
Nicolas G. Figueiredo <nicolasgregoriof@gmail.com>
2022-10-29 13:54:36 -0300

    primeiro commit

---
+++ tecnologia.js
@@ -0,0 +1,25 @@
+var nome, ano, twitter, facebook, instagram, linkedin, sexo, localizacao;
+
+nome = "Nicolas";
+document.write("Nome: ",nome, "<br>");
+
+ano = "2022";
+document.write("Ano: ",ano,"<br>");
+
+twitter = "@nicknick";
+document.write("Twitter: ",twitter,"<br>");
+
+facebook = "facebook.com/nicknick";
+document.write("Facebook: ",facebook,"<br>");
+
+instagram = "@nickcnick";
+document.write("Instagram: ",instagram,"<br>");
+
+linkedin = "linkedin/nicknick";
+document.write("Linkedin: ",linkedin,"<br>");
+
+sexo = "Masculino";
+document.write("Sexo: ",sexo,"<br>");
+
+localizacao = "Pirassununga - SP";
+document.write("Localização: ",localizacao,"<br>");
